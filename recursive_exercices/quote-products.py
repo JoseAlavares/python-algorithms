fc13626f665421068cd04cd0016f0f5151ac7698
Jose Francisco <alvaresvaldes89@outlook.es>
2022-12-30 16:58:40 -0600

    Recursive quote

---
+++ recursive_exercices/quote-products.py
@@ -0,0 +1,85 @@
+# Exercise to implement recursive functions in python and memoizing
+
+TAXES = [{
+    "tax_name": "iva",
+    "value": 16,# Representation in percent
+}, {
+    "tax_name": "ieps",# Impuesto Especial sobre Producción y Servicios (IEPS)
+    "value": 8,# Representation in percent
+}, {
+    "tax_name": "dta",# Derecho de Trámite Aduanero (DTA)
+    "value": 2,# Representation in percent
+}]
+quotes_to_calculate = [{
+    "user_id": 1,
+    "products": [{
+        "product_name": "laptop HP",
+        "price_before_taxes": 10233.00,
+    }, {
+        "product_name": "magic mouse",
+        "price_before_taxes": 102.00,
+    }, {
+        "product_name": "printer",
+        "price_before_taxes": 1012.12,
+    }, {
+        "product_name": "smart watch",
+        "price_before_taxes": 102.00,
+    }]
+}, {
+    "user_id": 2,
+    "products": [{
+        "product_name": "laptop DELL",
+        "price_before_taxes": 20119.79,
+    }, {
+        "product_name": "printer",
+        "price_before_taxes": 1012.12,
+    }, {
+        "product_name": "smart watch",
+        "price_before_taxes": 102.00,
+    }, {
+        "product_name": "web cam",
+        "price_before_taxes": 99.98,
+    }]
+}, {
+    "user_id": 3,
+    "products": [{
+        "product_name": "laptop MacBook Pro",
+        "price_before_taxes": 49999.99,
+    }, {
+        "product_name": "magic mouse",
+        "price_before_taxes": 102,
+    }]
+}]
+
+def iteration_over_user(quotes_to_calculate, results):
+    if len(quotes_to_calculate) == 0:
+        return results
+
+    user = quotes_to_calculate[0]
+    total = 0
+    total += calculate_total_per_user(user["products"], total)
+    results.append({"user_id": user["user_id"], "total": total })
+    return iteration_over_user(quotes_to_calculate[1:], results)
+
+def calculate_total_per_user(products, total):
+    if len(products) == 0:
+        return total
+    products[0]["price_before_taxes"] += calculate_taxes(products[0]["price_before_taxes"], TAXES, 0)
+    total += products[0]["price_before_taxes"] + calculate_total_per_user(products[1:], total)
+    return total
+
+def calculate_taxes(subtotal, taxes, total_taxes):
+    if len(taxes) == 0:
+        return total_taxes
+
+    tax = taxes[0]["value"]
+    total_taxes += (subtotal * tax)/100
+    return calculate_taxes(subtotal, taxes[1:], total_taxes)
+
+
+def main():
+    results = []
+    results_to_print = iteration_over_user(quotes_to_calculate, results)
+    print(results_to_print)
+
+main()
